869c838e9266ee0e1d80823c426372f12e5923ae
sirichandanamanduva <siri4b9@gmail.com>
2026-05-30 09:35:58 -0500

    arguments

--- 04-arguments.sh
+++ 04-arguments.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-person1=Sita
-person2=geeta
+person1=$1
+person2=$2
 
 echo "$person1 :: Hello $person2, how are you?"
 echo "$person2 :: Hi $person1, I'm fine thanks for asking what about you?"
 echo "$person1:: I'm good, what are you learning today?"
 echo "$person2 :: learning shell"
-
