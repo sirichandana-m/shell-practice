7eb54e98c0e546bafccf8f3ab78d54f1d976a094
sirichandanamanduva <siri4b9@gmail.com>
2026-05-30 17:11:04 -0500

    variables

---
+++ 07-specialvariables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#####special variables########
+
+echo " All args passed to script: $@ "
+echo " Number of variables passed to script: $# "
+echo " script name: $0 "
+echo " present directory: $PWD "
+echo " who is running: $USER "
+echo " Home directory of current user: $HOME"
+echo " PID of this script: $$"
+
+sleep 100 &
+
+echo " PID of recently executed background process: $!"
+echo " All args passed to script: $* "
