1791864edd330b3abb9d0d6ec2dfd81172b4b832
sirichandanamanduva <siri4b9@gmail.com>
2026-05-31 16:10:53 -0500

    conditions

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 USERID=$(id -u)
+
  if [ $USERID -ne 0 ]; then
  echo " please run the script with super user access "
  exit 1
  fi
+
  echo " Installing nginx "
+
 dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+echo " nginx installation is failure "
+exit 1
+fi 
+echo " nginx is installed successfully "
