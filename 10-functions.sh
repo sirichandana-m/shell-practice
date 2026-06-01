78ee611a35f6217fe8a1297b01266158fe60dd4d
sirichandanamanduva <siri4b9@gmail.com>
2026-06-01 08:59:44 -0500

    functions

---
+++ 10-functions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+echo " please run the script using root access"
+exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+echo "$2 ... failure "
+exit 1
+else
+echo "$2 ... success"
+fi
+
+}
+
+
+dnf install nginx -y
+VALIDATE $? "Installing nginx"
+
+
+dnf install mysql -y
+VALIDATE $? "Installing mysql"
+
+
+dnf install nodejs -y
+VALIDATE $? "Installing nodejs"
+
