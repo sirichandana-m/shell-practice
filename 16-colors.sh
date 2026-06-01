d322ad19858f43bd8edc2115f6a3861675106e7e
sirichandanamanduva <siri4b9@gmail.com>
2026-06-01 10:57:09 -0500

    conditions

--- 16-colors.sh
+++ 16-colors.sh
@@ -3,7 +3,10 @@
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
-
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ $USERID -ne 0 ]; then
 echo " please run the script using root access" | tee -a $LOGS_FILE
@@ -14,10 +17,10 @@ mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-echo "$2 ... failure " | tee -a $LOGS_FILE 
+echo -e "$2 ... $R failure $N " | tee -a $LOGS_FILE 
 exit 1
 else
-echo "$2 ... success" | tee -a $LOGS_FILE
+echo -e "$2 ... $G success $N" | tee -a $LOGS_FILE
 fi
 
 }
@@ -30,6 +33,6 @@ do
    dnf install $package -y &>>$LOGS_FILE
    VALIDATE $? "$package installation"
 else
-    echo "$package already installed, skipping"
+    echo -e "$package already installed, $Y skipping $N "
 fi
 done
