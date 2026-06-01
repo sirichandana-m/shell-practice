c43fb92499fd74e23e03875ad658d59c95ddbfdf
sirichandanamanduva <siri4b9@gmail.com>
2026-06-01 17:09:35 -0500

    conditions

--- 17-errorhandling.sh
+++ 17-errorhandling.sh
@@ -33,7 +33,7 @@ do
  if [ $? -ne 0 ];then
     echo "$package not installed, installing now"
    dnf install $package -y &>>$LOGS_FILE
-   VALIDATE $? "$package installation"
+   #VALIDATE $? "$package installation"
 else
     echo -e "$package already installed, $Y skipping $N "
 fi
