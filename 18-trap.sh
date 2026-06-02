401dbb5c208227709097ef1343d10277f706aba2
sirichandanamanduva <siri4b9@gmail.com>
2026-06-02 09:02:32 -0500

    conditions

--- 18-trap.sh
+++ 18-trap.sh
@@ -2,7 +2,7 @@
 
 set -e #ERR
 
-trap 'echo "There is error in the $LINENO, command: $BASH_COMMAND" ' $ ERR
+trap 'echo "There is error in the $LINENO, command: $BASH_COMMAND" ' ERR
 
 echo " Hello world"
 echo "I'm learning shell script"
